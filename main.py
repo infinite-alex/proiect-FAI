7299bd82da6f81a717d34397b89a3540dd3b4e5b
infinite-alex <113497896+infinite-alex@users.noreply.github.com>
2025-03-26 05:56:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import inversa as inv
 
+print("O zi frumoasa sa aveti! Acesta este proiectul nostru. Sper sa va placa...Acum putem trece la treaba.")
+print("Introduceti o matrice, dar nu prea mareeee...")
+
 m_1 = [ 
     [-1, 2, 1],
     [3, 0, 2],
